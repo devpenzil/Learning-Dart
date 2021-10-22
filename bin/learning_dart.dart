b9089193731d988d83f808f8ded14969f5e1f874
ajoalex012 <ajoalex012@gmail.com>
2021-10-22 20:36:34 +0530

    Day 2

--- bin/learning_dart.dart
+++ bin/learning_dart.dart
@@ -1,67 +1,30 @@
-import 'dart:io';
-
 void main() {
-  // print('hello world');
-  // var a = stdin.readLineSync();
-  // print('Hello $a');
+  // sumReq(fnum: 25, snum: 35);
+  // nest(10, 20, sumparams);
+  sumFuture(10, 20);
+}
 
-  // print('Enter two numbers');
-  // var input1 = stdin.readLineSync();
-  // var input2 = stdin.readLineSync();
+void sum() {
+  print(2 + 3);
+}
 
-  // var num1 = int.parse(input1!);
-  // var num2 = int.parse(input2!);
+void sumparams(int a, int b) {
+  print('${a + b}');
+}
 
-  // print('Sum = ${num1 + num2}');
+int sumreturn(int a, int b) {
+  return a + b;
+}
 
-  // int num = 0;
-  // if (num > 0) {
-  //   print('Number is a Positive');
-  // } else if (num < 0) {
-  //   print('Number is Negative');
-  // } else {
-  //   print('Number is 0');
-  // }
+void sumReq({required int fnum, required int snum, int tnum = 0}) {
+  print(fnum + snum + tnum);
+}
 
-  // for (var i = 0; i < 10; i++) {
-  //   print(i);
-  // }
+void nest(int a, int b, void Function(int, int) Customfun) {
+  Customfun(a, b);
+}
 
-  // List<int> listnumber = [1, 2, 223, 4, 5, 4];
-  // print(listnumber);
-
-  // var array = ['hello', 125, 20.0];
-  // print(array);
-
-  // var numberlist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  // var numberlist2 = [222, 3, 6, 8, 5, 4, 8];
-  // if (numberlist.contains(11)) {
-  //   print('Array has 5');
-  // } else {
-  //   print('Array Does not contain 5');
-  // }
-  // numberlist.add(33);
-  // print(numberlist);
-  // numberlist.removeAt(0);
-  // print(numberlist);
-
-  // numberlist.addAll(numberlist2);
-  // print(numberlist);
-  // print(numberlist.join('*'));
-
-  // List<List<int>> list = [
-  //   [1, 2, 3],
-  //   [4, 5, 6]
-  // ];
-
-  // print(list[0][1]);
-
-  // List<int> numberlist = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5];
-  // Set<int> numberset = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
-
-  // print(numberlist);
-  // print(numberset);
-
-  // var maps = {"name": "Ajo Alex", "age": 20};
-  // print(maps['name']);
+Future<int> sumFuture(int a, int b) async {
+  print('in Sum Future ${a + b}');
+  return a + b;
 }
